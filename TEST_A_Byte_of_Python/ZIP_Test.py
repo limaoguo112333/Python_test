f70472b2c89964363af17c2e4c91f9eeae7b42d0
limaoguo112333 <73691512+limaoguo112333@users.noreply.github.com>
2022-08-24 17:21:53 +0800

    Update ZIP_Test.py

--- TEST_A_Byte_of_Python/ZIP_Test.py
+++ TEST_A_Byte_of_Python/ZIP_Test.py
@@ -11,17 +11,17 @@ target_dir = 'F:/Visual_Studio/WorkSpace'
 if not os.path.exists(target_dir):
     os.mkdir(target_dir)
 
-today = target_dir + os.sep + time.strftime('%Y_%m_%d')
-now = time.strftime('Line3DPlot_%H_%M_%S')
+today = target_dir + os.sep + time.strftime('%Y_%m_%d') #命名存放文件夹
+now = time.strftime('Line3DPlot_%H_%M_%S')  #命名压缩文件
 target = today + os.sep + now + '.zip'
 
 if not os.path.exists(today):
     os.mkdir(today)
     print('Successfully created directory', today)
 
-zipFile = zipfile.ZipFile(target, 'w')
-for path, dirnames, filenames in os.walk(source):
+zipFile = zipfile.ZipFile(target, 'w')  #创建zipfile对象
+for path, dirnames, filenames in os.walk(source): #逐层遍历所有子文件夹
         fpath = path.replace(source, '')
         for filename in filenames:
-            zipFile.write(os.path.join(path, filename), os.path.join(fpath, filename))
+            zipFile.write(os.path.join(path, filename), os.path.join(fpath, filename))  #将每个文件写入zipfile对象下对应目录中
 zipFile.close()
